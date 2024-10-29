65a30b6b8739bbf8d108d9da2fc4feb848b05565
EYDER DE JESUS ARROYO VEGA <eyderarroyo19@gamil.com>
2024-10-28 19:42:10 -0500

    first commit

---
+++ Cara.java
@@ -0,0 +1,14 @@
+public class Cara {
+    public static void main(String[] args) {
+        // CARA CON JAVA
+
+        System.out.println("  @@@@@@@ ");
+        System.out.println(" @| 0 0 |@ ");
+        System.out.println(" (|  ^  |) ");
+        System.out.println("  | [_] |");
+        System.out.println(" *-------*");
+
+        System.out.println("Eyder Arroyo_DevSinior");
+    }
+
+}
